df128788679cda53a5df957d07686aed8eac774d
Nickguild1993 <nickguild@gmail.com>
2020-10-19 15:26:41 -0500

    added circleMarker progress

--- Mapping_Single_Points/Single_Points_Map/static/js/logic.js
+++ Mapping_Single_Points/Single_Points_Map/static/js/logic.js
@@ -4,15 +4,46 @@ console.log("working")
 // this will either confirm or deny that our logic.js
 // file is accessed in the console on Chrome.
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with a center (repped as 40.7, -94.5) and zoom level (repped as "4").
+// changed to 34.0522, -118.2437 for 13.4.1 to highligh LA
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+
+// addin a circle to LA // SKILL CHALLENGE! 13.4.2
+// .circle() uses  * meters *  for the radius!
+// L.circle([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: "yellow"
+// }).addTo(map);
+
+// Add a circle to the map using circleMarker() instead of circle() like you did above
+// ** THE DIFFERENCE BETWEEN THE 2 FUNCTIONS IS THAT CIRCLEMARKER USES PIXELS INSTEAD OF METERS
+
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: "yellow"
+}).addTo(map);
+
+// Add a marker to the map for LA, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// skill drill 13.4.1 editing to add in night mode 
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
+// We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Skill change for 13.4.1 - need to do night mode map , yellow circle with black outline.
--- simple_map/mod13_notes.md
+++ simple_map/mod13_notes.md
@@ -1,6 +1,6 @@
 # Notes for module 13
 
-13.2 - adding the map and such
+## 13.2 - adding the map and such
 
 refer to: https://leafletjs.com/examples/quick-start/
 
@@ -25,3 +25,48 @@ let map = L.map("mapid", {
 });
 
 This is useful for when we need to add multiple tile layers, or a background image of map(s)
+
+### Tile layer - still 13.2
+
+// We create the tile layer that will be the background of our map.
+
+let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: API_KEY
+});
+
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+
+BREAKDOWN OF ABOVE CODE: 
+
+1. We assign the tileLayer() method, as shown in the Quick Start Guide's "Setting up the map" section to the variable streets. Leaflet doesn't provide a tile layer. Instead, it offers various tile layer APIs.
+2. The following URLs appear in the parentheses of our tileLayer() method:
+- The API URL with a reference to the accessToken
+- The OpenStreetMap URL inside the curly braces of our tileLayer() method
+3. We add the maxZoom attribute and assign it a value of 18.
+4. We add the id attribute and assign it mapbox.streets, which will show the streets on the map.
+5. We add the accessToken attribute and assign it the value of our API_KEY.
+6. Finally, we call the addTo() function with our map object, map on our graymap object tile layer. The addTo() function will add the graymap object tile layer to our let map.
+- To change the map's style, change the map id using the list of Mapbox ids below
+    mapbox.streets, mabox.light, mapbox.dark etc.
+
+## Adding branches to github
+
+to change branches in the command line, use: git checkout <name of branch>
+
+### notes on creating a branch off of the main branch
+
+Navigate to your repository on your computer.
+
+Make sure you're on the main branch by typing: git branch
+
+If you're not on the main branch, type: git checkout main
+
+Pull the changes from the main branch by typing: git pull
+
+Create a new branch by typing: git checkout -b [name_of_your_new_branch]
